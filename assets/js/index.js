95a42631168eeea7c940aa418c102c020271631f
8Stringking <tomcondemned@hotmail.co.uk>
2022-02-13 03:01:46 +0000

    declared the variables for the game in javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,17 +6,17 @@ let playerOrder = [];
 let flash;
 //to keep track of what turn the user is on
 let turn;
-
+//a boolean on if the player is hitting the correct colours or not
 let good;
-
+//a boolean to keep track of weather its the players turn or the computers turn
 let compTurn;
 
 let intervalId;
-
+//this checks weather the strict box has been checked
 let strict = false;
-
+//this is for the noises that the panels make when flashing
 let noise = true;
-
+//checks if the power button has been turned on for the game
 let on = false;
-
+//tells us if the player has won the game or not
 let win;
