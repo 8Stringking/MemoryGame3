dde2354033fc963371afcc3ffb0c6b701c49253f
8Stringking <tomcondemned@hotmail.co.uk>
2022-02-13 03:01:08 +0000

    declared the variables for the game in javascript

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+//variable to keep track of the order of the lights
+let order = [];
+//the order that the player is pressing the lights in
+let playerOrder = [];
+//the number of flashes that have appeared in the gam
+let flash;
+//to keep track of what turn the user is on
+let turn;
+
+let good;
+
+let compTurn;
+
+let intervalId;
+
+let strict = false;
+
+let noise = true;
+
+let on = false;
+
+let win;
