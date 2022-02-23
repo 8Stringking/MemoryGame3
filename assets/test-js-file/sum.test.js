3bf7ba14aed1316fe9f105cda00930f0be05f921
8Stringking <tomcondemned@hotmail.co.uk>
2022-02-23 03:37:04 +0000

    added js test file for testing and run test for js file which is a pass  which was a pass for the game

--- assets/test-js-file/sum.test.js
+++ assets/test-js-file/sum.test.js
@@ -1,9 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
 
+ test('use jsdom in this test file', () => {
+    const element = document.createElement('div');
+    expect(element).not.toBeNull();
+  });
 
-const play = require("../js");
-
-test("a random number is multipled by 4 and adds 1", () => {
-    expect(play(1)).toBe(5)
-}); 
-
-module.exports = play
+  
+  
