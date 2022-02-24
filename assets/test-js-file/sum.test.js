09caec331cb76c4252415653bffc9da3a2074061
8Stringking <tomcondemned@hotmail.co.uk>
2022-02-24 00:20:35 +0000

    added unit test for accuracy button

--- assets/test-js-file/sum.test.js
+++ assets/test-js-file/sum.test.js
@@ -7,5 +7,11 @@
     expect(element).not.toBeNull();
   });
 
+const accuracyButton = require("../js")
+
+test ("accuracyButton when checked returns true")(); {
+  expect(accuracyButton.checked == true).toBe(accuracy = true)
+};
+
   
   
