1a1a7f6bfaba184ac059528374b7d5c8aca496ae
8Stringking <tomcondemned@hotmail.co.uk>
2022-02-24 02:54:28 +0000

    updated file path in testing file for js

--- assets/js/test-js-file/sum.test.js
+++ assets/js/test-js-file/sum.test.js
@@ -7,7 +7,7 @@
     expect(element).not.toBeNull();
   });
 
-const accuracyButton = require("../js")
+const accuracyButton = require("../test-js-file")
 
 test ("accuracyButton when checked returns true")(); {
   expect(accuracyButton.checked == true).toBe(accuracy = true)
